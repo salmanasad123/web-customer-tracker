2c981c9524c737aee9457ddf42c3e85c79d2a39f
salmanasad123 <slman.me@gmail.com>
2021-10-05 18:28:11 +0500

    Created CustomerDAO interface

---
+++ src/main/java/com/luv2code/springdemo/dao/CustomerDAO.java
@@ -0,0 +1,12 @@
+package com.luv2code.springdemo.dao;
+
+import com.luv2code.springdemo.entity.Customer;
+
+import java.util.List;
+
+// DAO is data access object, DAO will access database for us using hibernate API
+// DAO is responsible for interfacing with database
+public interface CustomerDAO {
+
+    public List<Customer> getCustomers();
+}
